05512af141a42bd70b7f54de46f9dabf1d48e85f
chiefkufre <samuelkufrewillie@gmail.com>
2023-03-12 22:55:19 +0100

    chore: fix db creation bug

--- core/database.py
+++ core/database.py
@@ -1,11 +1,18 @@
+import os
 from flask_sqlalchemy import SQLAlchemy
-
+from core.config import settings
 
 db = SQLAlchemy()
+DB_NAME = settings.DB_NAME
 
 
-
+# create database based on the model defined.
 def create_db(app):
-    db.app = app
-    db.init_app(app)
-    db.create_all()
+    # Check if the database file exists
+    if not os.path.exists("core" + DB_NAME):
+        # Set the application for the SQLAlchemy object
+        db.app = app
+        # Initialize the SQLAlchemy object with the applicatio
+        db.init_app(app)
+        # Create the database tables based on the defined models
+        db.create_all()
