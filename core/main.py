f074599ad7ee60aba0dfcd1624c5cd103973e3f1
chiefkufre <samuelkufrewillie@gmail.com>
2023-03-12 22:55:55 +0100

    chore: clean up the main.py file

--- core/main.py
+++ core/main.py
@@ -1,30 +1,66 @@
 from flask import Flask, redirect, render_template, request, session, url_for
-from flask_sqlalchemy import SQLAlchemy
-from core.config import settings 
 
-
+from core.config import settings
+from core.database import create_db
 
 
 # create application instance
 def create_app_instance():
-
     # initialize application
     app = Flask(__name__)
+    app.config.from_object(settings)
 
-    # add app configuration
-
-    app.config["SECRET"] = settings.SECRET_KEY
-    app.config['SESSION_TYPE'] = settings.SESSION_TYPE
-    app.config['SQLALCHEMY_DATABASE_URI'] = settings.DATABASE_URI
-    app.config['CKEDITOR_PKG_TYPE'] = settings.CKEDITOR
-
-    # initialize database
-    db = SQLAlchemy(app)
-   
+    with app.app_context():
+        # initialize database
+        create_db(app)
 
 
+
+    @app.route('/', methods=['GET', 'POST'])
+    def home():
+        return render_template('show.html')
 
     
+
+    @app.route('/register', methods=['GET', 'POST'])
+    def register():
+
+        if request.method == 'POST':
+            name = request.POST.get('name')
+            email = request.POST.get('email')
+            address = request.POST.get('address')
+            wsn = request.POST.get('tel')
+
+        return render_template('register.html')
+
+
+    @app.route('/login', methods=['GET', 'POST'])
+    def login():
+        return render_template('login.html')
+    
+
+
+    @app.route('/login', methods=['GET'])
+    def logout():
+        return redirect(url_for("home"))
+
+
+    @app.route('/create', methods=['GET','POST'])
+    def create_event():
+        return render_template('create.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     return app
-
-
