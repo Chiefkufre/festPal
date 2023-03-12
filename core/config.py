6a71ea1c8539830bcffc1f40a74e2fa03c7fc450
chiefkufre <samuelkufrewillie@gmail.com>
2023-03-12 22:54:56 +0100

    chore: refactor config file

--- core/config.py
+++ core/config.py
@@ -1,19 +1,27 @@
 from decouple import config
+from pathlib import Path
 
 
+# Use this to build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
-class Setting():
-    SECRET_KEY = config('SECRET_KEY')
-    DEBUG = config('DEBUG', default=False, cast=bool)
-    DB_NAME = config('DB_NAME')
+
+class Setting:
+    DEBUG = config("DEBUG", default=False, cast=bool)
+    SECRET_KEY = config("SECRET_KEY")
     SESSION_TYPE = "filesystem"
-    DATABASE_URI =  f'sqlite:///{DB_NAME}'
-    CKEDITOR = 'standard'
 
+    # database settings
+    DB_NAME = config("DB_NAME")
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{DB_NAME}"
+    CKEDITOR_PKG_TYPE = "standard"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    # static fir directory
+    STATIC_FOLDER: Path = BASE_DIR / "static"
 
-
+    # templates directory
+    TEMPLATES_FOLDER: Path = BASE_DIR / "templates"
 
 
 settings = Setting()
-
