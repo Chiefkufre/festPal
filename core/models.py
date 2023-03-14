7155e70b056a25cc7d648ed6bb747e5e70d49acf
chiefkufre <samuelkufrewillie@gmail.com>
2023-03-14 22:12:57 +0100

    chore: refactor model to represent listening party

--- core/models.py
+++ core/models.py
@@ -4,9 +4,12 @@ from sqlalchemy.types import Boolean
 
 from core.database import db
 
+from flask_login import UserMixin
+
 
 # User model that represents a user data
-class User(db.Model):
+class User(UserMixin,db.Model):
+
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
@@ -15,22 +18,54 @@ class User(db.Model):
     bio = db.Column(db.String(50), unique=False)
     profile_picture = db.Column(db.String(255))
     password = db.Column(db.String(200), nullable=False)
+    phone = db.Column(db.String(50), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(
         db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
     )
     is_active = db.Column(Boolean, default=True)
-    event = db.relationship("Event", backref="user", lazy=True)
+    virtual_party = db.relationship("VirtualListeningParty", backref="user", lazy=True)
+    
+
+    
 
 
-# Event model that represents events state
-class Event(db.Model):
-    __tablename__ = "events"
-
+# virtual listening party model
+class VirtualListeningParty(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    event_name = db.Column(db.String(100), nullable=False)
-    location = db.Column(db.String(100), nullable=False)
-    start_date = db.Column(db.String(50), nullable=False)
-    end_date = db.Column(db.String(50), nullable=False)
-    image = db.Column(db.String(255))
-    host = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(255))
+    link = db.Column(db.String(255))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(
+        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
+    )
+
+
+
+
+#  virtual room model
+class Room(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    sid = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(
+        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
+    )
+
+
+
+
+# the user model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    phone_number = db.Column(db.String(20), unique=True, nullable=False)
+    virtual_listening_party_id = db.Column(db.Integer, db.ForeignKey('virtual_listening_party.id'))
+    room_id = db.Column(db.Integer, db.ForeignKey('room.id'))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(
+        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
+    )
